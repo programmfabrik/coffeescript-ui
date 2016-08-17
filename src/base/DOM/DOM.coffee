programmfabrik/coffeescript-ui

commit b9d6c3a989480a2c6182339c9ca6b38e40530cf8

CUI.Demo
- group demos in core, base, extra, demo, test
- removed some obsolete and outdated demos
- added a "Layout" demo for Vertical/Horizontal Layout/List

CUI.Layout

- new parameter "auto_buttonbar": set the auto buttonbar
  behaviour default.

CUI.Form

- removed "ng" specific code from form table, we use overwrites
  in CSS now. hopefully form-tr-outer is won't be needed (cause its
  gone)

Theme.ng

- updates

src/base/DataField/DataField.coffee
@@ -413,6 +413,8 @@ class DataField extends DOM
 		@
 
 	setValue: (v, flags = {}) ->
+		if v == @getValue()
+			return
 		try
 			@checkValue(v, flags)
 		catch e

demo/Makefile
@@ -4,12 +4,11 @@ call_scss = sass --scss --no-cache --sourcemap=inline
 coffee_files = src/demo.coffee \
 	src/CUIDemo.coffee \
 	src/ControlsDemo.coffee \
-	src/CUIPlayground.coffee \
-	src/PaddingDemo.coffee \
 	src/MemoryTester.coffee \
 	src/BoundingBoxDemo/BoundingBoxDemo.coffee \
 	../src/base/DragDropSelect/demo/DragDropSelectDemo.coffee \
 	../src/base/Events/demo/EventsDemo.coffee \
+	../src/base/Layout/demo/LayoutDemo.coffee \
 	../src/base/XHR/demo/XHRDemo.coffee \
 	../src/elements/Button/demo/ButtonDemo.coffee \
 	../src/elements/Label/demo/LabelDemo.coffee \
@@ -17,16 +16,13 @@ coffee_files = src/demo.coffee \
 	../src/elements/ProgressMeter/demo/ProgressMeterDemo.coffee \
     ../src/elements/Button/demo/ButtonbarDemo.coffee \
 	../src/elements/Menu/demo/MenuDemo.coffee \
-	../src/elements/ItemList/demo/ItemListDemo.coffee \
 	../src/elements/Input/demo/InputDemo.coffee \
 	../src/elements/MarkdownInput/demo/MarkdownInputDemo.coffee \
 	../src/elements/FileUpload/demo/FileUploadDemo.coffee \
-	../src/elements/Modal/demo/ModalDemo.coffee \
+	../src/elements/Modal/demo/DialogDemo.coffee \
 	../src/elements/Popover/demo/PopoverDemo.coffee \
 	../src/elements/Tooltip/demo/TooltipDemo.coffee \
 	../src/elements/BorderLayout/demo/BorderLayoutDemo.coffee \
-	../src/elements/HorizontalLayout/demo/HorizontalLayoutDemo.coffee \
-	../src/elements/VerticalLayout/demo/VerticalLayoutDemo.coffee \
 	../src/elements/Pane/demo/PaneDemo.coffee \
 	../src/elements/Tabs/demo/TabsDemo.coffee \
 	../src/elements/Panel/demo/PanelDemo.coffee \
@@ -34,7 +30,7 @@ coffee_files = src/demo.coffee \
 	../src/elements/Form/demo/FormDemo.coffee \
 	../src/elements/Form/demo/FormLayoutDemo.coffee \
 	../src/elements/ListView/demo/ListViewDemo.coffee \
-	../src/elements/ListView/demo/TreeListViewDemo.coffee \
+	../src/elements/ListView/demo/ListViewTreeDemo.coffee \
 	../src/elements/DigiDisplay/demo/DigiDisplayDemo.coffee
 
 jpg_files = ../src/elements/Menu/demo/Schmetterling.jpg
@@ -62,7 +58,7 @@ html:
 	@cat ../src/base/*/demo/*html > $(target)/easydbui_demo.html
 	@cat ../src/elements/*/demo/*html >> $(target)/easydbui_demo.html
 
-css_ng:
+css_ng: jpg
 	#
 	# $@
 	mkdir -p $(target)/css
@@ -77,7 +73,7 @@ css_other:
 	$(call_scss) src/scss/themes/dark/main.scss $(target)/css/cui_demo_dark.css
 
 
-css: css_ng css_other jpg
+css: css_ng css_other
 
 jpg: $(jpg_files)
 	#

demo/src/BoundingBoxDemo/BoundingBoxDemo.coffee
@@ -1,4 +1,7 @@
 class BoundingBoxDemo extends Demo
+	getGroup: ->
+		"Demo"
+
 	display: ->
 		@bb = $div("cui-bounding-box-demo-bounding-box")
 		@bb_inner = $div("cui-bounding-box-demo-inner")

demo/src/CUIDemo.coffee
@@ -1,4 +1,10 @@
 class CUIDemo extends Demo
+	getName: ->
+		"Form Elements"
+
+	getGroup: ->
+		"Test"
+
 	display: ->
 
 		# output a control bar

demo/src/CUIPlayground.coffee
@@ -1,313 +1,0 @@
-class CUIPlayground extends Demo
-	constructor: (@opts={}) ->
-		super(@opts)
-		@__game_steps = []
-		@__games = []
-		@__games_data =
-			per_game: []
-			game: 0
-
-	addGame: (game) ->
-		CUI.debug "adding game", game,  @__games.length, @getGameByName(game.name())
-		if @getGameByName(game.name())
-			CUI.alert(text: "game already exists.")
-			return
-		game.idx = @__games.length
-		@__games.push(game)
-		@__games_data.per_game[game.idx] = {}
-		@
-
-	getGameByName: (name) ->
-		for game in @__games
-			CUI.debug game.name(), name
-			if game.name() == name
-				return game
-		null
-
-	display: ->
-		@__playground = $div("cui-playground-playground cui-playground-allow-cursor", "cui-playground-cursor": 0)
-		@__playground_cursor = $div("cui-playground-cursor").appendTo(@__playground)
-		Events.listen
-			type: "click"
-			node: @__playground_cursor[0]
-			call: (ev) =>
-				@__playground_cursor.detach()
-				@__playground.addClass("cui-playground-select-cursor")
-				ev.stopPropagation()
-				@renderGameControl()
-				for el in DOM.findElements(@__playground[0], ".cui-playground-allow-cursor")
-					cursor_idx = DOM.getAttribute(el, "cui-playground-cursor")
-					cs_div = $div("cui-playground-cursor-select")
-					do (cursor_idx) =>
-						Events.listen
-							type: "click"
-							node: cs_div
-							call: (ev) =>
-								@__playground.removeClass("cui-playground-select-cursor")
-								@__playground.find(".cui-playground-cursor-select").remove()
-								@setCursor(cursor_idx)
-								ev.stopPropagation()
-								return
-
-					el.appendChild(cs_div[0])
-				return
-
-		@__cursor_places = 1
-
-		Events.listen
-			type: "click"
-			call: (ev) =>
-				target = ev.getTarget()
-				# CUI.debug "clicked on", ev.getTarget(), DOM.findElement(@__playground[0], ":focus")
-
-		@addGame(new VerticalLayoutPlaygroundGame(playground: @))
-		@addGame(new LabelPlaygroundGame(playground: @))
-
-		@__right_layout = new VerticalLayout
-			top:
-				flexHandle:
-					hidden: false
-					closed: false
-			center:
-				content: $text("center")
-
-		@__layout = new HorizontalLayout
-			center:
-				content: @__playground
-			right:
-				flexHandle:
-					hidden: false
-					closed: false
-				content: @__right_layout
-
-		@renderGameControl()
-
-		CUI.DOM.waitForDOMInsert(node: @__layout.DOM[0])
-		.done =>
-			@setCursor(0)
-
-		@__layout
-
-
-	renderGameControl: (_step) ->
-		CUI.debug "render game control", _step
-
-		if not isNull(_step)
-			# ok jQuery is shorter for this...
-			@__playground.find(".cui-playground-game-has-control").removeClass("cui-playground-game-has-control")
-
-			game_div = DOM.findElement(@__playground[0], "[cui-playground-game-step=\"#{_step}\"]")
-			game_div.classList.add("cui-playground-game-has-control")
-			@__playground.addClass("cui-playground-a-game-has-control")
-			@__playground.removeClass("cui-playground-no-game-has-control")
-		else
-			@__playground.addClass("cui-playground-no-game-has-control")
-			@__playground.removeClass("cui-playground-a-game-has-control")
-
-		game_control = new VerticalLayout
-			top:
-				content: null
-
-		update_game_options = (game, data) =>
-			fields = game.getFields()
-			for f in fields
-				f.undo_and_changed_support = false
-				if f.default
-					data[f.name] = f.default
-					delete(f.default)
-				if not f.form
-					f.form = {}
-				if not f.form.label
-					f.form.label = f.name
-
-			form = new Form
-				fields: fields
-				data: data
-			.start()
-			game_control.replace(form, "center")
-
-
-		if isNull(_step)
-			game_opts = []
-			for game, idx in @__games
-				game_opts.push
-					value: idx
-					text: game.name()
-
-			game_control_form = new Form
-				data: @__games_data
-				fields: [
-					type: Form
-					horizontal: true
-					fields: [
-						type: Select
-						options: game_opts
-						name: "game"
-						onDataChanged: =>
-							game = @__games[@__games_data.game]
-							update_game_options(game, @__games_data.per_game[game.idx])
-					,
-						type: FormButton
-						text: "Add"
-						onClick: =>
-							game = @__games[@__games_data.game]
-							@__game_steps.push
-								cursor: @getCursor()
-								game: game.name()
-								data: copyObject(@__games_data.per_game[game.idx], true)
-
-							@renderPlayground()
-					]
-				]
-
-			update_game_options(@__games[0], @__games_data.per_game[0])
-
-		else
-			step = @__game_steps[_step]
-			game_data = copyObject(step.data)
-			game = @getGameByName(step.game)
-			game_control_form = new Form
-				horizontal: true
-				fields: [
-					type: Output
-					text: game.name()
-				,
-					type: FormButton
-					text: "Update"
-				]
-			update_game_options(game, game_data)
-
-		game_control.replace(game_control_form.start(), "top")
-		@__right_layout.replace(game_control, "top")
-		game_control
-
-
-	setCursor: (@__cursor=0) ->
-		cursor_div = DOM.findElement(@__layout.DOM[0], "[cui-playground-cursor=\"#{@__cursor}\"]")
-		if not cursor_div
-			CUI.alert(text: "Cursor DIV not found.")
-		else
-			cursor_div.appendChild(@__playground_cursor[0])
-		@
-
-	getCursor: ->
-		@__cursor
-
-	renderPlayground: ->
-		CUI.debug "render playground", dump(@__game_steps)
-		@__playground.empty()
-		@__cursor_places = 1
-
-		for step, step_idx in @__game_steps
-			@setCursor(step.cursor)
-			game = @getGameByName(step.game)
-			if not game
-				CUI.alert(text: "Game not found: "+step.game)
-				continue
-			@renderGame(game, step.data, step_idx)
-
-		@setCursor(@__cursor_places-1)
-
-	renderGame:(game, data, step_idx) ->
-		div = game.render(data)
-		div.classList.add("cui-playground-game")
-		DOM.setAttribute(div, "cui-playground-game-step", step_idx)
-
-		Events.listen
-			type: "click"
-			node: div
-			call: (ev) =>
-				if div.classList.contains("cui-playground-game-has-control")
-					div.classList.remove("cui-playground-game-has-control")
-					@renderGameControl()
-					CUI.debug "clicked on game second time", step_idx, DOM.data(div)
-				else
-					@renderGameControl(step_idx)
-					CUI.debug "clicked on game", step_idx, DOM.data(div)
-				ev.stopPropagation()
-				return
-
-		for el in DOM.findElements(div, ".cui-playground-allow-cursor")
-			DOM.setAttribute(el, "cui-playground-cursor", @__cursor_places++)
-
-		DOM.data div,
-			game: game
-			data: data
-
-		assert(CUI.DOM.isInDOM(@__playground_cursor[0]), "CUIPlayground.renderGame", "Cursor needs to be in DOM.")
-		@__playground_cursor.before(div)
-		@
-
-
-class PlaygroundGame extends Element
-	initOpts: ->
-		super()
-		@addOpts
-			playground:
-				mandatory: true
-				check: CUIPlayground
-
-	name: ->
-		"<game>"
-
-	render: (data) ->
-		assert(false, @__cls+".render", "Needs implementation.")
-
-	getFields: ->
-		[]
-
-	# set by the playground
-	idx: -1
-
-class VerticalLayoutPlaygroundGame extends PlaygroundGame
-	name: ->
-		"VerticalLayout"
-
-	getFields: ->
-		fields = []
-		for k in ["top", "center", "bottom"]
-			fields.push
-				type: Input
-				name: k
-				default: k
-				form:
-					label: k
-		fields.push
-			type: Checkbox
-			name: "maximize"
-			default: true
-			form:
-				label: "maximize"
-		fields
-
-	render: (data) ->
-		new VerticalLayout
-			maximize: data.maximize
-			top:
-				content: $text(data.top)
-			center:
-				class: "cui-playground-allow-cursor"
-				content: $text(data.center)
-			bottom:
-				content: $text(data.bottom)
-		.DOM[0]
-
-
-class LabelPlaygroundGame extends PlaygroundGame
-	getFields: ->
-		fields = [
-			type: Input
-			name: "text"
-			default: "Label"
-		]
-
-	name: ->
-		"Label"
-
-	render: (data) ->
-		new Label
-			text: data.text
-		.DOM[0]
-
-
-Demo.register(new CUIPlayground())

demo/src/ControlsDemo.coffee
@@ -1,4 +1,6 @@
 class ControlsDemo extends Demo
+	getGroup: ->
+		"Test"
 
 	addControlExample: (text,control) ->
 		control.addClass("cui-control")

demo/src/MemoryTester.coffee
@@ -1,4 +1,6 @@
 class MemoryTester extends Demo
+	getGroup: ->
+		"Test"
 
 	display: ->
 		@vl = new VerticalLayout(top: {})

demo/src/PaddingDemo.coffee
@@ -1,262 +1,0 @@
-
-class PaddingDemo extends Demo
-
-    getName: () ->
-        "Padding"
-
-    display: ->
-
-        @demo_table = new DemoTable("cui-padding-demo")
-
-        @demo_table.addDivider("Horizontal padding examples")
-
-        layout =
-            new HorizontalLayout
-                maximize: false
-                class: "cui-padding-outer-bigger"
-                left:
-                    content: new Label( text: "1" )
-                center:
-                    content: new Label( text: "2" )
-                right:
-                    content: [
-                        new HorizontalLayout
-                            maximize: false
-                            left:
-                                content: new Label( text: "3.1" )
-                            center:
-                                content: new Label( text: "3.2" )
-                            right:
-                                content: new Label( text: "3.3 Reset", class: "pad-reset" )
-                    ]
-        @demo_table.addExample("cui-padding-outer-bigger", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout =
-            new HorizontalLayout
-                maximize: false
-                class: "cui-padding-outer-bigger"
-                left:
-                    content: new Label( text: "1" )
-                center:
-                    content: new Label( text: "2" )
-                right:
-                    content: [
-                        new HorizontalLayout
-                            maximize: false
-                            left:
-                                content: new Label( text: "3.1" )
-                            center:
-                                content: new Label( text: "3.2" )
-                    ]
-        @demo_table.addExample("cui-padding-outer-bigger", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            maximize: false
-            class: "cui-padding-outer-bigger"
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: new Label( text: "2" )
-            right:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-#                        class: "cui-padding-outer-big"
-#                        left:
-#                            content: new Label( text: "3.1" )
-                        center:
-                            content: new Label( text: "3.1" )
-#                        right:
-#                            content: new Label( text: "3.3" )
-                ]
-        @demo_table.addExample("cui-padding-outer-bigger", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            maximize: false
-
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: new Label( text: "2" )
-            right:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-                        class: "cui-padding-outer-bigger"
-#                        left:
-#                            content: new Label( text: "3.1" )
-                        center:
-                            content: new Label( text: "3.1" )
-#                        right:
-#                            content: new Label( text: "3.3" )
-                ]
-        @demo_table.addExample("nested cui-padding-outer-bigger ", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            maximize: false
-            class: "cui-padding-outer-bigger"
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: new Label( text: "2" )
-            right:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-                        class: "cui-padding-outer-big"
-#                        left:
-#                            content: new Label( text: "3.1" )
-                        center:
-                            content: [
-                                new HorizontalLayout
-                                    maximize: false
-#class: "cui-padding-outer-big"
-                                    center:
-                                        content: new Label( text: "3.1.1" )
-                            ]
-#                        right:
-#                            content: new Label( text: "3.3" )
-                ]
-        @demo_table.addExample("outer big inside outer bigger padding", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            maximize: false
-            class: "cui-padding-outer-bigger"
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-                        class: "cui-padding-outer-big"
-#                        left:
-#                            content: new Label( text: "3.1" )
-                        center:
-                            content: [
-                                new HorizontalLayout
-                                    maximize: false
-#class: "cui-padding-outer-big"
-                                    center:
-                                        content: new Label( text: "2.1.1" )
-                            ]
-#                        right:
-#                            content: new Label( text: "3.3" )
-                ]
-            right:
-                content: new Label( text: "3" )
-        @demo_table.addExample("outer big inside outer bigger padding", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            class: "cui-padding-outer-bigger"
-            maximize: false
-            center:
-                content: new Label( text: "1" )
-        @demo_table.addExample("cui-padding-outer-bigger, one cell", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-
-        layout = new HorizontalLayout
-            class: "cui-padding-outer-bigger"
-            maximize: false
-            center:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-                        center:
-                            content: new Label( text: "1.1" )
-                ]
-        @demo_table.addExample("cui-padding-outer-bigger, one cell", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            class: "cui-padding-outer-bigger"
-            maximize: false
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-                        left:
-                            content: new Label( text: "2.1" )
-                        center:
-                            content: [
-                                new HorizontalLayout
-                                    maximize: false
-                                    left:
-                                        content: new Label( text: "2.1.1" )
-                                    center:
-                                        content: new Label( text: "2.1.2" )
-                            ]
-                ]
-            right:
-                content: new Label( text: "3" )
-
-        @demo_table.addExample("cui-padding-outer-bigger, one cell", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            class: "cui-padding-outer-bigger"
-            maximize: false
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: [
-                    new HorizontalLayout
-                        maximize: false
-                        left:
-                            content: new Label( text: "2.1" )
-                        center:
-                            content: [
-                                new HorizontalLayout
-                                    maximize: false
-                                    left:
-                                        content: new Label( text: "2.1.1" )
-                                    center:
-                                        content: [
-                                            new HorizontalLayout
-                                                maximize: false
-                                                left:
-                                                    content: new Label( text: "2.1.2.1" )
-                                                center:
-                                                    content: new Label( text: "2.1.2.1" )
-                                        ]
-                            ]
-                ]
-            right:
-                content: new Label( text: "3" )
-
-        @demo_table.addExample("cui-padding-outer-bigger, one cell", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-        layout = new HorizontalLayout
-            class: "cui-padding-outer-bigger"
-            absolute: true
-            maximize: true
-            left:
-                content: new Label( text: "1" )
-            center:
-                content: new Label( text: "2" )
-            right:
-                content: new Label( text: "3" )
-        @demo_table.addExample("cui-padding-outer-bigger, absolute", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-
-        # ========================================================================================================================================
-
-        @demo_table.addDivider("Horizontal list")
-
-        layout = new HorizontalList
-            class: "cui-padding-outer-bigger"
-            maximize: false
-            content: [
-                new Label( text: "1" )
-                new Label( text: "2" )
-                new Label( text: "3" )
-                new Label( text: "4" )
-            ]
-
-        @demo_table.addExample("cui-padding-outer-bigger, one cell", $div("cui-padding-demo-reset-width").append(layout.DOM) )
-
-
-
-        @demo_table.table
-
-
-Demo.register(new PaddingDemo())

commit f13a16aa10603373591e10ea683fa3cc70c2cf00

CUI.DOM
- getDimensions: added "offsetTop" and "offsetLeft" (also "Scaled" variantes)

CUI.DataField

- fixed "setValue": this did not return the DataField, if values
  has not changed. bug introduced by b9d6c3a989480a2c6182339c9ca6b38e40530cf8.

src/base/DOM/DOM.coffee
@@ -734,6 +734,8 @@ class CUI.DOM extends Element
 		for k in [
 			"offsetWidth"
 			"offsetHeight"
+			"offsetTop"
+			"offsetLeft"
 			"clientWidth"
 			"clientHeight"
 			"scrollWidth"
@@ -748,6 +750,7 @@ class CUI.DOM extends Element
 
 		for k in [
 			"offsetWidth"
+			"offsetLeft"
 			"clientWidth"
 			"scrollWidth"
 			"scrollLeft"
@@ -756,6 +759,7 @@ class CUI.DOM extends Element
 
 		for k in [
 			"offsetHeight"
+			"offsetTop"
 			"clientHeight"
 			"scrollHeight"
 			"scrollTop"

src/base/DataField/DataField.coffee
@@ -414,13 +414,13 @@ class DataField extends DOM
 
 	setValue: (v, flags = {}) ->
 		if v == @getValue()
-			return
+			return @
 		try
 			@checkValue(v, flags)
 		catch e
 			if e not instanceof CheckValueError
-				throw e
-			throw new Error("#{@__cls}.setValue: "+v+", Error: "+e)
+				throw(e)
+			throw(new Error("#{@__cls}.setValue: "+v+", Error: "+e))
 
 		if flags.no_trigger not in [false, true]
 			flags.no_trigger = true