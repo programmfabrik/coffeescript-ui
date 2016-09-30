programmfabrik/coffeescript-ui

commit b3c10559d675871241057946bd7e7c392ae313dc

ListViewTree
- "setFather" with new father "null", deselect node in current Tree.
- "removeFromDOM", deselect node when removing self.

This fixes problems which can occurr later when another node is selected,
but the old selected node is no longer in DOM.

src/elements/ListView/ListViewTreeNode.coffee
@@ -54,6 +54,10 @@ class ListViewTreeNode extends ListViewRow
 		if new_father
 			assert(@ not in new_father.getPath(true), "#{getObjectClass(@)}.setFather", "father cannot any of the node's children", node: @, father: new_father)
 
+		if not new_father and @selected
+			@getRoot()?.selectedNode = null
+			@selected = false
+
 		if @father and not new_father
 			# copy tree, since we are a new root node
 			tree = @getTree()
@@ -210,14 +214,20 @@ class ListViewTreeNode extends ListViewRow
 		else
 			@do_open = false
 
-		if @element and remove_self
-			if @getRowIdx() == null
-				if not @isRoot()
-					@getTree().removeDeferredRow(@)
-			else
-				@getTree().removeRow(@getRowIdx())
+		if remove_self
 
-			@element = null
+			if @selected
+				@getRoot()?.selectedNode = null
+				@selected = false
+
+			if @element
+				if @getRowIdx() == null
+					if not @isRoot()
+						@getTree().removeDeferredRow(@)
+				else
+					@getTree().removeRow(@getRowIdx())
+
+				@element = null
 
 		@is_open = false
 		@

commit c4f14bf4c990deed1f2dd98a318796e9d6cfb810

Makfile
- Improvements to "all" target.

ListView

- Fixed a problem introduced in b3c10559d675871241057946bd7e7c392ae313dc,
  when we remove a node from dom, we don't necessarily want to deselect it.
  Do this only when the node is removed from the tree by "setFather(null)".

Makefile
@@ -163,12 +163,14 @@ html_files = \
 	src/elements/StickyHeader/StickyHeader.html \
 	src/elements/Tabs/Tab.html
 
-all: code font_awesome demo css
+all: code demo css
 	#
 	# $@
 	mkdir -p $(target)
 
 font_awesome:
+	#
+	# $@
 	mkdir -p $(target)
 
 	rm -rf $(target)/font-awesome

src/elements/ListView/ListViewTreeNode.coffee
@@ -216,10 +216,6 @@ class ListViewTreeNode extends ListViewRow
 
 		if remove_self
 
-			if @selected
-				@getRoot()?.selectedNode = null
-				@selected = false
-
 			if @element
 				if @getRowIdx() == null
 					if not @isRoot()