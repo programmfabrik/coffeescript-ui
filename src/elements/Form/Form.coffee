programmfabrik/coffeescript-ui

commit e050208707326189dfb6c5d3671482eacd980aba

Layer
- Use better variable when positioning layer in "ng". This fixes
  a problem where the layer would get "left" and "right" at the
  same time.

Form

- reworked logic when form includes a checkbox, new classes
- don't render inline form, if it has no fields

src/elements/Form/Form.coffee
@@ -514,7 +514,9 @@ class CUI.Form extends CUI.DataField
 			CUI.DOM.addClass(container, @_class_table)
 
 		get_append = (v, info=@) =>
-			if CUI.isPlainObject(v) # assume a label constructor
+			if v instanceof Form and v.getFields().length == 0
+				null
+			else if CUI.isPlainObject(v) # assume a label constructor
 				# new Label(v).DOM
 				new MultilineLabel(v).DOM
 			else if isString(v)
@@ -588,10 +590,10 @@ class CUI.Form extends CUI.DataField
 							node: cb
 							call: =>
 								if cb.getValue()
-									blk.removeClass("cui-form--block-content-hidden")
+									blk.addClass("cui-form-block--checkbox-checked")
 									field.show()
 								else
-									blk.addClass("cui-form--block-content-hidden")
+									blk.removeClass("cui-form-block--checkbox-checked")
 									field.hide()
 					left_side = cb
 				else
@@ -607,10 +609,12 @@ class CUI.Form extends CUI.DataField
 					]
 
 				if cb
+					blk.addClass("cui-form-block--has-checkbox")
+
 					if cb.getValue()
-						blk.removeClass("cui-form--block-content-hidden")
+						blk.addClass("cui-form-block--checkbox-checked")
 					else
-						blk.addClass("cui-form--block-content-hidden")
+						blk.removeClass("cui-form-block--checkbox-checked")
 
 				append(blk)
 

src/base/Layer/Layer.coffee
@@ -874,26 +874,26 @@ class CUI.Layer extends CUI.DOM
 
 			switch vp.align_horizontal
 				when "left"
-					left = vp.layer_pos.left
+					pos_left = vp.layer_pos.left
 				when "right"
-					right = dim_window.width - (vp.layer_pos.left + vp.layer_pos.width)
+					pos_right = dim_window.width - (vp.layer_pos.left + vp.layer_pos.width)
 				when "center"
-					left = vp.layer_pos.left
+					pos_left = vp.layer_pos.left
 
 			switch vp.align_vertical
 				when "top"
-					top = vp.top
+					pos_top = vp.top
 				when "bottom"
-					bottom = dim_window.height - (vp.layer_pos.top + vp.layer_pos.height)
+					pos_bottom = dim_window.height - (vp.layer_pos.top + vp.layer_pos.height)
 				when "center"
-					top = vp.layer_pos.top
+					pos_top = vp.layer_pos.top
 
 			# set layer
 			CUI.DOM.setStyle @__layer.DOM,
-				top: top
-				bottom: bottom
-				left: left
-				right: right
+				top: pos_top
+				bottom: pos_bottom
+				left: pos_left
+				right: pos_right
 
 				# width: Math.ceil(vp.layer_pos.width)
 				# height: Math.ceil(vp.layer_pos.height)

commit 2b3522eee7021cd5ee99039263b290bc1c167380

Form
- Fixed output of FormPopover within a Form. This
  got broken in e050208707326189dfb6c5d3671482eacd980aba.

src/elements/Form/Form.coffee
@@ -467,6 +467,13 @@ class CUI.Form extends CUI.DataField
 
 		return false
 
+	hasContentForAppend: ->
+		console.error "Form:", @, "hasContentForAppend", @__fields
+		if @__fields?.length > 0
+			true
+		else
+			false
+
 	__renderTableNg: ->
 
 		layout = @getLayout()
@@ -514,7 +521,7 @@ class CUI.Form extends CUI.DataField
 			CUI.DOM.addClass(container, @_class_table)
 
 		get_append = (v, info=@) =>
-			if v instanceof Form and v.getFields().length == 0
+			if v instanceof Form and not v.hasContentForAppend()
 				null
 			else if CUI.isPlainObject(v) # assume a label constructor
 				# new Label(v).DOM

src/elements/FormPopover/FormPopover.coffee
@@ -90,6 +90,9 @@ class FormPopover extends Form
 
 		@registerTemplate(vl.getLayout())
 
+	hasContentForAppend: ->
+		# FormPopver always shows a button
+		true
 
 	render: ->
 		button_opts = copyObject(@_button, true)