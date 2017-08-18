902469d875dc9ed078cae60aa359fd2307d5f325
Roman <roman@programmfabrik.de>
2017-08-18 09:24:34 +0200

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ConcatPlugin = require('webpack-concat-plugin');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const htmlFiles = require('./html-files');
+
+const config = {
+    context: APP_DIR,
+    entry: './index.coffee',
+    output: {
+        path: BUILD_DIR,
+        filename: 'cui.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.coffee/,
+                loader: 'coffee-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{loader: 'css-loader'}, {loader: 'sass-loader'}]
+                })
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(BUILD_DIR),
+        new ExtractTextPlugin("css/cui_ng.css"),
+        new CopyWebpackPlugin(
+            [
+                {from: '../thirdparty/marked/lib/marked.js', to: 'js'},
+                {from: '../thirdparty/moment', to: 'js'},
+                {from: 'scss/icons/icons.svg', to: 'css/icons.svg'}
+            ]
+        ),
+        new ConcatPlugin({
+            fileName: 'cui.html',
+            filesToConcat: htmlFiles
+        })
+    ]
+};
+
+module.exports = config;
