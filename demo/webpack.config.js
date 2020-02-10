2b655132180db4970b6d1a075a904d536bc651e5
Stefanie Hein <steffi@the-elements.de>
2020-02-10 20:49:53 +0100

    DEMO: upgrade webpack to v4, refactor the CSS extraction into separate theme files with miniCssExtractPlugin (it replaces the ExtractTextWebpackPlugin which should not be used for CSS anymore with wp4).

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,30 +1,60 @@
 const path = require('path');
-const webpack = require('webpack');
 
-const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname + path.sep, 'public');
 const APP_DIR = path.resolve(__dirname + path.sep, 'src');
-
-const extractSass = new ExtractTextWebpackPlugin("cui-demo.css");
-const ngExtractSass = new ExtractTextWebpackPlugin("css/cui_ng.css");
-const fylrExtractSass = new ExtractTextWebpackPlugin("css/cui_fylr.css");
-const debugExtractSass = new ExtractTextWebpackPlugin("css/cui_debug.css");
 
 module.exports = function(env) {
     const isProduction = !!(env && env.production);
 
     return {
+        mode: isProduction ? 'production' : 'development',
         context: APP_DIR,
-        entry: './index.coffee',
+        entry: {
+            main_js: './index.coffee',
+            cui_demo: APP_DIR + '/scss/demo.scss',
+            cui_fylr: '../../src/scss/themes/fylr/main.scss', 
+            cui_debug: '../../src/scss/themes/debug/main.scss',             
+            cui_ng: '../../src/scss/themes/ng/main.scss', 
+        },
         output: {
             path: BUILD_DIR,
-            filename: 'cui-demo.js'
+            filename: (chunkData) => {   
+                // we cannot prevent wp from generating a js output for each css only entry,
+                // each emitted js file must therefore have a unique filename to prevent error: "Multiple chunks emit assets to the same filename"
+                if (chunkData.chunk.id === 'main_js') {
+                    return 'cui-demo.js';              
+                } else {
+                    return '[name].js'
+                }
+            },            
         },
+        optimization: {
+            namedChunks: true, // needed so we can use [id].css to name the extracted css files,
+            minimize: isProduction,
+            minimizer: [
+                new TerserPlugin({
+                    cache: true,
+                    parallel: true,
+                    // sourceMap: true, // Must be set to true if using source-maps in production
+                    extractComments: false,
+                    terserOptions: {
+                        // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                        warnings: false,
+                        keep_classnames: true,
+                        keep_fnames: true,
+                    }
+                }),
+            ],   
+        },        
         devtool: (!isProduction ? 'source-map' : undefined),
         devServer: {
             publicPath: '/public/',
-            // hot: true, // ExtractTextWebpackPlugin does not support HMR
+            writeToDisk: true, // needs to be turned on because the theme files are loaded from disk and not from an inline <style>
         },
         module: {
             rules: [
@@ -33,61 +63,21 @@ module.exports = function(env) {
                 loader: 'coffee-loader'
             },
             {
-                test: /demo\.scss$/,
-                use: extractSass.extract({
-                    use: [
-                        { loader: "css-loader", options: { sourceMap: true } },
-                        { loader: "postcss-loader", options: { 
-                            config: { path: __dirname, ctx: { optimize: isProduction } },
-                            sourceMap: true,
-                        }},
-                        { loader: "sass-loader", options: { sourceMap: true } }, 
-                    ]
-                })
-            },
-            {
-                test: /themes\/ng\/.*\.scss$/,
-                use: ngExtractSass.extract({
-                    use: [
-                        { loader: "css-loader", options: { sourceMap: true } },
-                        { loader: "postcss-loader", options: { 
-                            config: { path: __dirname, ctx: { optimize: isProduction } },
-                            sourceMap: true,
-                        }},
-                        { loader: "sass-loader", options: { sourceMap: true } }, 
-                    ]
-                })
-            },
-            {
-                test: /themes\/fylr\/.*\.scss$/,
-                use: fylrExtractSass.extract({
-                    use: [
-                        { loader: "css-loader", options: { sourceMap: true } },
-                        { loader: "postcss-loader", options: { 
-                            config: { path: __dirname, ctx: { optimize: isProduction } },
-                            sourceMap: true,
-                        }},
-                        { loader: "sass-loader", options: { sourceMap: true } }, 
-                    ]
-                }),
-            },
-            {
-                test: /themes\/debug\/.*\.scss$/,
-                use: debugExtractSass.extract({
-                    use: [
-                        { loader: "css-loader", options: { sourceMap: true } },
-                        { loader: "postcss-loader", options: { 
-                            config: { path: __dirname, ctx: { optimize: isProduction } },
-                            sourceMap: true,
-                        }},
-                        { loader: "sass-loader", options: { sourceMap: true } }, 
-                    ]
-                })
-            },
+                test: /\.scss$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'postcss-loader', options: { 
+                        config: { path: __dirname, ctx: { optimize: isProduction } },
+                        sourceMap: true,
+                    }},
+                    { loader: 'sass-loader', options: { sourceMap: true } }, 
+                ],
+            },            
             {
                 test: /(icons\.svg|\.txt|\.json)/,
                 loader: 'raw-loader'
-            },
+            },            
             {
                 test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
                 exclude: /icons\.svg/,
@@ -100,14 +90,20 @@ module.exports = function(env) {
             ]
         },
         plugins: [
-            extractSass,
-            ngExtractSass,
-            fylrExtractSass,
-            debugExtractSass,
+            new FixStyleOnlyEntriesPlugin(), // Removes extraneous js files that are emitted from the scss only entries
+            new MiniCssExtractPlugin({ 
+                moduleFilename: ({ id }) => {
+                    if ( id === 'cui_demo') {
+                        return 'cui-demo.css';
+                    } else {
+                        return '/css/[id].css';
+                    }
+                },
+            }),
             new webpack.ProvidePlugin({
                 'Demo': APP_DIR + '/Demo.coffee',
                 'CUI': 'coffeescript-ui/public/cui.js'
-            })
-        ]
+            }),
+        ],
     };
 };
