c8769bfeaf937acbd32d44c692dfb69b161dc74d
Roman <roman.glombovsky@programmfabrik.de>
2022-06-01 14:26:47 +0100

    Updated public js files

--- public/cui.js
+++ public/cui.js
@@ -57879,10 +57879,12 @@ CUI.CodeInput = (function(superClass) {
     this.__aceEditor.getSession().setMode("ace/mode/" + this._mode);
     value = (ref = this.__data) != null ? ref[this._name] : void 0;
     if (value) {
-      try {
-        value = JSON.parse(value);
-        value = JSON.stringify(value, null, '\t');
-      } catch (error) {}
+      if (CUI.util.isPlainObject(value)) {
+        try {
+          value = JSON.parse(value);
+        } catch (error) {}
+      }
+      value = JSON.stringify(value, null, '\t');
       this.__aceEditor.setValue(value, -1);
       this.__aceEditor.clearSelection();
     }
